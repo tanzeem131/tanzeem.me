b6b2535ccc58e0ef9e5f91e0740142d2d09608ca
Tanzeem <dev.tanzeem@gmail.com>
2025-01-02 01:02:27 +0530

    Google analytics embedded

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Navbar from "@/components/navbar";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Timeline from "@/components/timeline";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function Home() {
   return (
     <>
+      <GoogleAnalytics gaId="G-07XVVKRJY1" />
       <Navbar />
       <About />
       <Skills />
